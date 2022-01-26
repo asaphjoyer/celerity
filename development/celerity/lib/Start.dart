6c64b0e7ddabf37d575692372e510677ec23f809
asaph_joyer <asaph1390@gmail.com>
2022-01-26 14:19:27 +0530

    google auth

---
+++ development/celerity/lib/Start.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/auth.dart';
+import 'package:pixel_perfect/pixel_perfect.dart';
+
+class Start extends StatelessWidget {
+  const Start({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        actions: [
+          IconButton(
+              onPressed: () => FirebaseAuth.instance.signOut(),
+              icon: Icon(Icons.logout))
+        ],
+      ),
+    );
+  }
+}
